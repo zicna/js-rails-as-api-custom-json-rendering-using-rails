5a1fb01af2511beefe0c6bbb0fd9d5c6d1e1d13f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-02 11:12:58 -0500

    commit tree ad664b0cc2ce15c3e43ee99d0b576ee98a6b22aa

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,19 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: @birds
+    # render json: birds, only: [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+    # render json: {id: bird.id, name: bird.name, species: bird.species}
+    render json: bird.slice(:id, :name, :species)
+    #slice returns new hash only with keys we pass 
+    else
+      render json: {message: "Bird not found"}
+    end
   end
 end
